61a04c2ad55ae0d3ea0c120d97fbdfcfd84b198b
equivalence1 <equivalence1@gmail.com>
2017-03-19 23:05:44 +0300

    aborting build in case of fail

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 make clean
-make
-sudo chown 0:0 bin/aucont_start
-sudo chmod u+s bin/aucont_start
+make || exit 1
+sudo chown 0:0 bin/aucont_start || exit 1
+sudo chmod u+s bin/aucont_start || exit 1
