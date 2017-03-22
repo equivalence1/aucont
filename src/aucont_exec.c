b7f52b47b34676609af6e0434e9d781c1f8d08d2
equivalence1 <equivalence1@gmail.com>
2017-03-22 23:07:14 +0300

    joining cgroup

--- src/aucont_exec.c
+++ src/aucont_exec.c
@@ -10,6 +10,7 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 static
 int join_ns(int pid, const char *ns_name)
@@ -19,11 +20,57 @@ int join_ns(int pid, const char *ns_name)
 
     int fd = open(ns, O_RDONLY);
     if (fd < 0) {
-        pr_err("Could not open %s\n", ns);
+        pr_warn("Could not open %s\n", ns);
         goto err;
     }
     if (setns(fd, 0) < 0) {
-        pr_err("Could not setns %s\n", ns);
+        pr_warn("Could not setns %s\n", ns);
+        goto err;
+    }
+    close(fd);
+
+    pr_success("Joined %s ns\n", ns);
+    return 0;
+
+err:
+    print_errno();
+    return -1;
+}
+
+static
+int go_to_process_ns(int pid)
+{
+    if (join_ns(pid, "uts") < 0)
+        return -1;
+    if (join_ns(pid, "net") < 0)
+        return -1;
+    if (join_ns(pid, "ipc") < 0)
+        return -1;
+    if (join_ns(pid, "pid") < 0)
+        return -1;
+    if (join_ns(pid, "user") < 0)
+        return -1;
+    if (join_ns(pid, "mnt") < 0)
+        return -1;
+    return 0;
+}
+
+static
+int join_cgroup(int pid)
+{
+    char cg[100];
+    snprintf(cg, sizeof cg, "/tmp/aucont_cgroup_cpu/cg_%d/tasks", pid);
+
+    char my_pid_str[10];
+    snprintf(my_pid_str, sizeof my_pid_str, "%d", getpid());
+
+    int fd = open(cg, O_WRONLY);
+    if (fd < 0) {
+        pr_err("Could not open %s\n", cg);
+        goto err;
+    }
+    if (write(fd, my_pid_str, strlen(my_pid_str)) < 0) {
+        pr_err("Could not join cgroup of %d\n", pid);
         goto err;
     }
     close(fd);
@@ -35,31 +82,15 @@ err:
     return -1;
 }
 
-
-static
-int go_to_process_ns(int pid)
-{
-    if (join_ns(pid, "net") < 0)
-        return -1;
-    if (join_ns(pid, "ipc") < 0)
-        return -1;
-    if (join_ns(pid, "user") < 0)
-        return -1;
-    if (join_ns(pid, "pid") < 0)
-        return -1;
-    if (join_ns(pid, "uts") < 0)
-        return -1;
-    if (join_ns(pid, "mnt") < 0)
-        return -1;
-    return 0;
-}
-
 int main(int argc, char *argv[])
 {
     setuid(0);
 
     int pid = atoi(argv[1]);
     char *cmd = argv[2];
+
+    if (join_cgroup(pid) < 0)
+        exit(EXIT_FAILURE);
 
     if (go_to_process_ns(pid) < 0)
         exit(EXIT_FAILURE);
@@ -77,6 +108,7 @@ int main(int argc, char *argv[])
     }
 
     if (pid == 0) {
+        pr_success("%s", "Executing user's command\n");
         if (execve(cmd, args, NULL) < 0) {
             pr_err("%s", "Could not exec\n");
             print_errno();
